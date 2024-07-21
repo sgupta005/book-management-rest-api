839c53136cde34de07f6eb9c33d981746c63002b
Shivam Gupta <shivamgupta02005@gmail.com>
2024-07-21 20:13:23 +0530

    Create user Schema

---
+++ src/user/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      trim: true,
+      lowercase: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avatar: {
+      type: String, //cloudinary url
+    },
+    booksList: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+      },
+    ],
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
