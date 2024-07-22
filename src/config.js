6a62274a812609128323f14ab20078eebdd7b583
Shivam Gupta <shivamgupta02005@gmail.com>
2024-07-22 15:54:25 +0530

    Create function to upload file to cloudinary

--- src/config.js
+++ src/config.js
@@ -14,6 +14,9 @@ const _config = {
   accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY,
   refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
   refreshTokenExpiry: process.env.REFRESH_TOKEN_EXPIRY,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);
---
+++ src/service/cloudinary.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+async function uploadOnCloudinary(localFilePath) {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: 'auto',
+    });
+    console.log('File has been uploaded to cloudinary successfully');
+    console.log(response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+}
+
+export { uploadOnCloudinary };
